0b2e2dbbbc5c6f0c27163d4507e92f7787166abd
kaklakariada <kaklakariada@users.noreply.github.com>
2019-01-06 12:11:56 +0100

    Fix issues calling when the lambda via API Gateway

--- src/main/java/org/itsallcode/openfasttrace/lambda/TraceLambda.java
+++ src/main/java/org/itsallcode/openfasttrace/lambda/TraceLambda.java
@@ -1,5 +1,7 @@
 package org.itsallcode.openfasttrace.lambda;
 
+import java.util.function.Supplier;
+
 import javax.inject.Inject;
 
 import org.itsallcode.openfasttrace.lambda.service.OftService;
@@ -12,20 +14,20 @@ import com.github.kaklakariada.aws.lambda.controller.RequestHandlerMethod;
 
 public class TraceLambda extends LambdaRequestHandler
 {
-    protected TraceLambda()
+    public TraceLambda()
     {
         super(new TraceController(), new OftServiceFactory());
     }
 
-    private static class TraceController implements LambdaController
+    public static class TraceController implements LambdaController
     {
         @Inject
-        private OftService oftService;
+        private Supplier<OftService> oftService;
 
         @RequestHandlerMethod
         public TraceResponse trace(@RequestBody TraceRequest request)
         {
-            return oftService.trace(request);
+            return oftService.get().trace(request);
         }
     }
 }
